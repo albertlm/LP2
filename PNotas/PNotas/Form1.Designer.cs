fe8dc46ab948dd20a65a22e48687282b45aa8b0b
Albert Leme <81541740+albertlm@users.noreply.github.com>
2023-08-02 21:08:51 -0300

    Add files via upload

---
+++ PNotas/PNotas/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace PNotas
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.lbxAlunos = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(46, 197);
+            this.btnVerificar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(238, 166);
+            this.btnVerificar.TabIndex = 0;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // lbxAlunos
+            // 
+            this.lbxAlunos.FormattingEnabled = true;
+            this.lbxAlunos.ItemHeight = 18;
+            this.lbxAlunos.Location = new System.Drawing.Point(322, 18);
+            this.lbxAlunos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lbxAlunos.Name = "lbxAlunos";
+            this.lbxAlunos.Size = new System.Drawing.Size(508, 526);
+            this.lbxAlunos.TabIndex = 1;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(843, 554);
+            this.Controls.Add(this.lbxAlunos);
+            this.Controls.Add(this.btnVerificar);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmPrincipal";
+            this.Text = "PNotas";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.ListBox lbxAlunos;
+    }
+}
+
---
+++ PNotas/PNotas/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace PNotas
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            lbxAlunos.Items.Clear();
+            //RA Albert: 003482123001
+            int i, j;
+            string auxiliar;
+            double[,] notas = new double[1, 10];
+            string[] gabarito = new string[10] { "A", "B", "C", "D", "A", "B", "C", "D", "A", "B" };
+            string[] respostas = new string[10];
+
+            //Recebimento das respostas dos alunos
+            for (i = 0; i < 1; i++)
+            {
+                for ( j= 0; j < 10; j++)
+                {
+                    respostas[j] = Interaction.InputBox("Resposta da questão " + (j + 1) + " do aluno " + (i + 1));
+                    if (!((respostas[j] == "A") || (respostas[j] == "B") || (respostas[j] == "C") || (respostas[j] == "D") || (respostas[j] == "E")))
+                    {
+                        MessageBox.Show("Valor inválido");
+                        j -= 1;
+                    }
+                }
+            }
+
+            //Comparação respostas e gabarito
+            for (i = 0; i < 1; i++)
+            {
+                for (j = 0; j < 10; j++)
+                {
+                    if (respostas[j] == gabarito[j])
+                    {
+                        lbxAlunos.Items.Add("Aluno " + (i + 1) + " acertou questão " + (j + 1) + " era " + (gabarito[j]) + " escolheu " + (respostas[j]) + "\n");
+                    }
+                    else
+                    {
+                        lbxAlunos.Items.Add("Aluno " + (i + 1) + " errou questão " + (j + 1) + " era " + (gabarito[j]) + " escolheu " + (respostas[j]) + "\n");
+                    }
+
+                }
+            }
+        }
+    }
+}
